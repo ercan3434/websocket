42a909edf671f3ca83d937ab81674433b0f394d6
ercan3434 <ercancimen3434@gmail.com>
2026-03-16 16:14:51 +0300

    start nextjs websocket

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/socket/:path*",
+        destination: "https://websocket-seven-nu.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
